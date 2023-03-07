8e8c2713aa2c24d393ca1b8a5e7a0db17d666ff4
lopo <lopo@zju.edu.cn>
2023-03-07 14:43:02 +0800

    feat: definition

--- src/utils.rs
+++ src/utils.rs
@@ -17,6 +17,18 @@ pub struct KeyCombination {
     pub extra: Option<ExtraKey>,
 }
 
+/// 按键事件 (目标键 + 辅助键 + 按键状态)
+#[napi(object)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+pub struct KeyEv {
+    /// 目标键 (可用值见 mapper 文件)
+    pub key: String,
+    /// 辅助键 见[ExtraKey]
+    pub extra: Option<ExtraKey>,
+    /// 是否是按下状态 (默认为 `false`)
+    pub down: Option<bool>,
+}
+
 /// 坐标
 #[napi(object)]
 #[derive(Debug, Eq, PartialEq, Hash, Clone)]
